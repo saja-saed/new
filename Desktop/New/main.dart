25200470c45fe95c1f8a7d10b12401d899a32e37
saja-saed <96354185+saja-saed@users.noreply.github.com>
2022-02-23 20:12:11 +0200

    Add files via upload

---
+++ Desktop/New/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        alignment: Alignment(-0.90, -0.90),
+        child: Text(
+          "01",
+          style: TextStyle(fontSize: 100, color: Colors.black12),
+        ),
+      ),
+    ),
+  ));
+}
